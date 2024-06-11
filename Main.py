cddc4af6ea96e361886b1f64d2fa0421430aa219
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-06-11 12:56:37 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -45,7 +45,7 @@ custom_css = """
 openprocessing_iframe = """
     <div class="background">
         
-        <iframe src="https://openprocessing.org/sketch/2295511/embed/" width="400" height="400"></iframe>
+        <iframe src="https://openprocessing.org/sketch/2295511/embed/" width="100%" height="100%"></iframe>
         
     </div>
 """
