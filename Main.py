cea81c5d5478c33814298d59a707bdd772f33b0b
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-06-11 12:52:57 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ custom_css = """
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     .stApp {
-        background: rgba(255,255,255,0.8);
+        background: rgba(255,255,255,0.3);
     }
     .background {
         position: fixed;
